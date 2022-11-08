4fd10c0d6d1687f6fcc83fa066e8c3c68750059e
Nufail <nufailihsan.ruzain@gmail.com>
2022-11-08 07:19:02 +0700

    nufail

---
+++ VS CODE file.py
@@ -0,0 +1,26 @@
+print ("Hello world to phyton")
+
+print (1+1)
+print (2-2)
+print (3*3)
+print (4//4)
+
+a=20
+b=10
+
+print (a//b)
+print (a/b)
+
+usia = 16 
+kata = ('usiaku: 16 tahun')
+
+print ('usiaku: 16 tahun')
+
+
+
+
+
+
+
+
+
